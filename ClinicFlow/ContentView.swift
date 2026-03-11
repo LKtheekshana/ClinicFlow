97b0a5e68bdab27d67f6dd9e31b7d37df6427337
dinethchandima <dinethpabasara2095@gmail.com>
2026-03-11 12:39:58 +0530

    test pc d

--- ClinicFlow/ContentView.swift
+++ ClinicFlow/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Dineth3!")
+            Text("Hello, Dineth3chandima")
         }
         .padding()
     }
