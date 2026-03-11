8912dbd5ab3456468b1795386f80edffc57269d1
Vindya-Su <104778468+Vindya-Su@users.noreply.github.com>
2026-03-11 20:03:20 +1300

    test3

--- ClinicFlow/ContentView.swift
+++ ClinicFlow/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Dineth2!")
+            Text("Hello, Dineth3!")
         }
         .padding()
     }
