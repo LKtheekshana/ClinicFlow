adfb76e5ae1038e3f62e636d3e85f07779f55973
dineth <dinethpabasara2095@gmail.com>
2026-03-11 12:15:36 +0530

    Initial Commit

---
+++ ClinicFlow/ClinicFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicFlowApp.swift
+//  ClinicFlow
+//
+//  Created by COBSCCOMP242P-033 on 2026-03-11.
+//
+
+import SwiftUI
+
+@main
+struct ClinicFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
